16a3ee0c6556fb95d0a78ace9b3e90337303098c
darwin-rod <darwin.rodriguezme@ug.edu.ec>
2024-05-30 21:53:00 -0500

    Python dates

---
+++ 00_dates.py
@@ -0,0 +1,29 @@
+### Dates ###
+
+from datetime import datetime
+
+def print_date(now):
+    print(now.year)
+    print(now.month)
+    print(now.day)
+    print(now.hour)
+    print(now.minute)
+    print(now.second)
+    print(now.timestamp())
+
+
+now = datetime.now()
+
+print_date(now)
+
+## Creo fecha ##
+year_2025 = datetime(2025, 1, 1, 18, 51, 12)
+
+print_date(year_2025)
+
+from datetime import time
+
+current_time = time()
+
+current_time
+
